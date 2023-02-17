7e008a562f23a94cf71ebee0b360eb3df4ab10dd
BabiychukYuliya <julia4233097@gmail.com>
2023-02-17 13:52:00 +0200

    second task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import Profile  from "./Profile/Profile";
-import user from './Profile/user'
+import user from './Profile/user';
+
+import Statistics from "./Statistics/Statistics";
+import data from './Statistics/data'
 
 export const App = () => {
   return (
@@ -11,6 +14,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
 
     </div>
   );
