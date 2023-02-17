a55175caf8c0abbbeb61a7b4cb58b279e1cffd46
BabiychukYuliya <julia4233097@gmail.com>
2023-02-17 21:27:54 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,12 +22,12 @@ export const App = () => {
       />
 
       <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
+      {/* <Statistics stats={data} /> */}
 
       <FriendList friends={friends} />
 
       <TransactionHistory items={transactions} />
-      
+
     </div>
   );
 };
